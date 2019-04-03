6f1039bbe7c754281f42ee93948dc6781ccefcc1
Paul Tobin <tobster111@gmail.com>
2019-04-03 15:32:14 +0100

    Refactored to add a season config with text and iconName

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
-const getSeason = (lat, month) => {
-  if (month > 2 && month < 9) {
-    return lat > 0 ? 'summer ☀️' : 'winter ❄️';
-  } else {
-    return lat < 0 ? 'winter ❄️' : 'summer ☀️';
+const seasonConfig = {
+  summer: {
+    text: `Lets hit the beach! ☀️`,
+    iconName: 'sun'
+  },
+  winter: {
+    text: `Burr, it's chilly! ❄️`,
+    iconName: 'snowflake'
   }
 };
 
-const SeasonDisplay = ({ lat, long }) => {
-  const season = getSeason(lat, new Date().getMonth());
-  const text =
-    season === 'winter' ? `Lets hit the beach! ☀️` : `Burr, it's chilly! ❄️`;
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
 
-  return <div>{text}</div>;
+const SeasonDisplay = ({ lat }) => {
+  const season = getSeason(lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div>
+      <i className={`${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
