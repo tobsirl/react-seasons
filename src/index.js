e0f1e9883be5be99bd4fca7758283a96872661ae
Paul Tobin <tobster111@gmail.com>
2019-04-03 11:38:24 +0100

    Testing componentDidMount + componentDidUpdate

--- src/index.js
+++ src/index.js
@@ -27,6 +27,14 @@ class App extends Component {
     );
   }
 
+  componentDidMount() {
+    console.log('My component was rendered to the screen');
+  }
+
+  componentDidUpdate() {
+    console.log('My component was just updated - it rerendered');
+  }
+
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
