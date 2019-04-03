f98591bd14218ddde936cf623baaeb4e88588897
Paul Tobin <tobster111@gmail.com>
2019-04-03 11:15:09 +0100

    Added state to the component. Changed the state using setState

--- src/index.js
+++ src/index.js
@@ -2,12 +2,46 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      long: null,
+      errorMessage: ''
+    };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
+      position => {
+        console.log(position);
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        });
+      },
+      err => {
+        this.setState({
+          errorMessage: err.message
+        });
+      }
     );
-    return <div>Latitude: </div>;
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <div>
+          Latitude: {this.state.lat} <br />
+          Longitude: {this.state.long}
+        </div>
+      );
+    }
+
+    return <div>Loading!...</div>;
   }
 }
 
