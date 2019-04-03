06ff1b572076d43bb76259430270ca49340002e0
Paul Tobin <tobster111@gmail.com>
2019-04-03 13:40:33 +0100

    SeasonDisplay determines if the its summer or winter

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer ☀️' : 'winter ❄️';
+  } else {
+    return lat < 0 ? 'winter ❄️' : 'summer ☀️';
+  }
+};
+
 const SeasonDisplay = ({ lat, long }) => {
-  return (
-    <div>
-      Latitude: {lat} <br />
-      Longitude: {long}
-    </div>
-  );
+  const season = getSeason(lat, new Date().getMonth());
+  const text =
+    season === 'winter' ? `Lets hit the beach! ☀️` : `Burr, it's chilly! ❄️`;
+
+  return <div>{text}</div>;
 };
 
 export default SeasonDisplay;
